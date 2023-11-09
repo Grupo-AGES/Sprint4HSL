28300891580c4ff6cb596694a51b9c27ebea88d2
KillianDB <killiandomingues57@gmail.com>
2023-11-09 17:01:42 -0300

    base incial componente background

---
+++ src/components/Background/index.tsx
@@ -0,0 +1,12 @@
+import {View, Image, Text, Linking, TouchableOpacity} from 'react-native';
+
+import styles from './backgroundStyle';
+
+export function Background() {
+const Background = {}
+  return(
+    <Background>
+        
+    </Background>
+  )
+}
